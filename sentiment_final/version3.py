e894fd14369ea7baa49d708a8b85d766e6d5b676
rithasyed <ritha.s@tringapps.com>
2024-10-10 18:16:15 +0530

    Update version3.py

--- sentiment_final/version3.py
+++ sentiment_final/version3.py
@@ -10,7 +10,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import concurrent.futures
 from wordcloud import WordCloud
-import matplotlib.colors as mcolors
 
 logging.basicConfig(level=logging.INFO)
 
@@ -255,22 +254,6 @@ def summarize_with_openai(summary):
 
     return analyzer.LLMClient(prompt)
 
-def hex_to_RGB(hex_str):
-    """ #FFFFFF -> [255,255,255]"""
-    return [int(hex_str[i:i+2], 16) for i in range(1,6,2)]
-
-def get_color_gradient(c1, c2, n):
-    """
-    Given two hex colors, returns a color gradient
-    with n colors.
-    """
-    assert n > 1
-    c1_rgb = np.array(hex_to_RGB(c1))/255
-    c2_rgb = np.array(hex_to_RGB(c2))/255
-    mix_pcts = [x/(n-1) for x in range(n)]
-    rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
-    return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
-
 def create_sentiment_chart(sentiment_dist):
     colors = ['#ff6361', '#ffa600', '#bc5090']
     fig = go.Figure(data=[go.Pie(
@@ -322,17 +305,13 @@ def create_category_bar_and_keyword_cloud(data):
         height=400,
         width=600
     )
-
-    # Prepare data for keyword word cloud
     keyword_freq = {}
     for category in data['top_categories_and_keywords']:
         for keyword in category['top_keywords']:
             keyword_freq[keyword['keyword']] = keyword['occurrences']
 
-    # Create word cloud
     wordcloud = WordCloud(width=600, height=400, background_color='white').generate_from_frequencies(keyword_freq)
 
-    # Create matplotlib figure for word cloud
     fig_cloud, ax = plt.subplots(figsize=(10, 6))
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis('off')
